5b23dcdb2eb4bce2aad22cc80d515e230c8bd283
Arian Sanchez <arian.sanchez@vallegrande.edu.pe>
2019-04-17 09:50:05 -0500

    (vistas) Inst Personal List Personal
    (modelo)
    Personal

---
+++ Edooc/src/java/modelo/Personal.java
@@ -0,0 +1,18 @@
+package modelo;
+
+import lombok.Data;
+
+@Data
+public class Personal {
+    private int idPer;
+    private String nomPer;
+    private String apePer;
+    private String corPer;
+    private String dniPer;
+    private String docPer;
+    private String celPer;
+    private String ubiPer;
+    
+    
+    
+}
