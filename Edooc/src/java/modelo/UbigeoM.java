495bc320caac8f76f229991f3c4f351dcfee0b11
Juan Valerio <jvaleriom@vallegrande.edu.pe>
2019-05-17 20:32:58 -0500

    edooca

---
+++ Edooc/src/java/modelo/UbigeoM.java
@@ -0,0 +1,44 @@
+package modelo;
+
+
+public class UbigeoM {
+    
+    String CODUBI;
+    String DISUBI;
+    String PROUBI;
+    String DEPUBI;
+
+    public String getCODUBI() {
+        return CODUBI;
+    }
+
+    public void setCODUBI(String CODUBI) {
+        this.CODUBI = CODUBI;
+    }
+
+    public String getDISUBI() {
+        return DISUBI;
+    }
+
+    public void setDISUBI(String DISUBI) {
+        this.DISUBI = DISUBI;
+    }
+
+    public String getPROUBI() {
+        return PROUBI;
+    }
+
+    public void setPROUBI(String PROUBI) {
+        this.PROUBI = PROUBI;
+    }
+
+    public String getDEPUBI() {
+        return DEPUBI;
+    }
+
+    public void setDEPUBI(String DEPUBI) {
+        this.DEPUBI = DEPUBI;
+    }
+    
+    
+}
